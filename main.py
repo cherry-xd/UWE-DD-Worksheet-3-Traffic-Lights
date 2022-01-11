c402c9fca2a272e408deae7c3bb3f34088cfd01e
Luke Emrys Walker <81542899+cherry-xd@users.noreply.github.com>
2022-01-11 17:06:20 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from dd_gpio import DD_GPIO, GPIO_INPUT, GPIO_OUTPUT
+import time
+
+if __name__ == '__main__':
+
+    gpio = DD_GPIO()
+    pin = GPIO_OUTPUT
+
+    gpio.write(pin.ADC0, pin.HIGH)
+    gpio.write(pin.ADC1, pin.HIGH)
+
+    while True:
+        state = int(gpio.read(GPIO_INPUT.ADC7)
+
+        if(state):
+            gpio.write(pin.ADC0, pin.HIGH)
+            gpio.write(pin.ADC1, pin.LOW) 
+            time.sleep(2)
+            gpio.write(pin.ADC0, pin.LOW)
+            gpio.write(pin.ADC1, pin.HIGH)
+            time.sleep(3)
+            gpio.write(pin.ADC0, pin.LOW)
+            gpio.write(pin.ADC1, pin.LOW)
+            time.sleep(2)
+            gpio.write(pin.ADC0, pin.HIGH)
+            gpio.write(pin.ADC1, pin.HIGH)
+            time.sleep(3)
+
